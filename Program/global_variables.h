30902ad074f9520b0c6b96462472bc33c7856d3e
CurtisHorton <49759737+CurtisHorton@users.noreply.github.com>
2019-04-30 13:40:18 -0400

    Add files via upload
    Added calibrate_camera() which allows us to calibrate the cameras rotation if we put a sheet of graph paper aligned with the bed

--- Program/global_variables.h
+++ Program/global_variables.h
@@ -6,6 +6,9 @@
 extern const int height; //image height
 extern const int width; //image width
 extern const double part_height; //input part height
-extern const double pen_z_offset; //pen height offset in mm from nozzle head
+extern const double pen_z_offset; //pen height offset in mm from camera
+extern const double pen_y_offset; //pen y-axis offset in mm from camera
+extern const double pen_x_offset; //pen x-axis offset in mm from camera
+extern const double full_vision_height; //height in mm that camera can view the full bed
 
 #endif
