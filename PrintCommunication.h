86f460562e2e0e480ce45696b3415769f982f508
Hugo Provencher <hugo1507@msn.com>
2019-05-03 19:16:55 -0400

    Add files via upload

--- PrintCommunication.h
+++ PrintCommunication.h
@@ -11,75 +11,73 @@
 //#include "CXBOXController.h"
 
 #include <iostream>
-#include "tserial.h"
-#include "tserial.cpp"
+#include<strstream>
+#include "serial_com.h"
 #include <bitset>
-#include <string>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+
+#include <windows.h>
 
 using namespace std;
 
 
 class PComm{
 private:
-	int Comm;
-	Tserial *Port;
+	//char Port[13];
+	HANDLE h1;
+	const int NMAX = 64;
+	char buffer[NMAX];
+	
 public:
-	PComm(int Com);
+	PComm(/*int Com*/);
+	~PComm();
 
 	void Send_Pos(double Pos[3]);
 	void Home();
 };
 
-PComm::PComm(int Com){
-	char Commu = (char)Com;
-	char *Commun = &Commu;
-	Comm = Com;
-	Port = new Tserial();
-	Port->connect(const_cast<char*>(Commun), 9600, spNONE);
+PComm::PComm(){
+	//open_serial("\\\\.\\COM3", h1);
+}
+
+PComm::~PComm(){
 }
 
 void PComm::Send_Pos(double Pos[3]){
-	char *Position;
-	string PPos[3], Full;
-	PPos[0] = to_string(Pos[0]);
-	PPos[1] = to_string(Pos[1]);
-	PPos[2] = to_string(Pos[2]);
+	//ostrstream sout(buffer, NMAX);
 
-	
-	Full = 'X';
-	Full += PPos[0];
-	Full += 'Y';
-	Full += PPos[1];
-	Full += 'Z';
-	Full += PPos[2];
-	
-	Position = new char[Full.length() + 1];
+	//char *Position;
+	//string PPos[3], Full;
+	//PPos[0] = to_string(Pos[0]);
+	//PPos[1] = to_string(Pos[1]);
+	//PPos[2] = to_string(Pos[2]);
 
-	std::copy(Full.begin(), Full.end(), Position);
+	//
+	//Full = 'X';
+	//Full += PPos[0];
+	//Full += 'Y';
+	//Full += PPos[1];
+	//Full += 'Z';
+	//Full += PPos[2];
+	//
+	//Position = new char[Full.length() + 1];
 
-	Position[Full.length()] = '\0';
-	
-	cout << Position <<endl;
-	Port->sendArray(Position, Full.length());
+	//std::copy(Full.begin(), Full.end(), Position);
 
-	delete Position;
+	//Position[Full.length()] = '\0';
+	//
+	//cout << Position <<endl;
+	////Port->sendArray(Position, Full.length());
+
+	//delete Position;
 }
 
 void PComm::Home(){
-	char *Position;
 
 
-	Position = new char[4];
-
-	Position[0] = 'G';
-	Position[1] = '2';
-	Position[2] = '8';
-	Position[3] = '\0';
-
-	cout << Position << endl;
-	Port->sendArray(Position, 4);
-
-	delete Position;
+	//sout
 }
 #endif
